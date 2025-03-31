c941b9f76759e2c33ffe7da82868dece83d3249c
MirkanPolat <Mirkanpolat@outlook.de>
2025-03-31 10:51:49 +0200

    adding some features like progressbar

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let currentQuestion = 0; // wir starten bei 0 
+let rightQuestions = 0; 
 
 function init(){
     document.getElementById("all-questions").innerHTML = questions.length // ladet die fragenlänge rein 
@@ -8,8 +9,19 @@ function init(){
 function showQuestion(){
 
     if(currentQuestion >= questions.length){
-        document.getElementById("next-button").disabled = true;
+        document.getElementById("endScreen").style = "";
+        document.getElementById("questionBody").style = "display: none";
+
+        document.getElementById("amount-Of-Questions").innerHTML = questions.length;
+        document.getElementById("amount-Of-right-Questions").innerHTML = rightQuestions;
+        document.getElementById("header-image").src = "./img/win.jpg";
     }else{
+
+        let percent = (currentQuestion + 1) / questions.length;
+        percent = Math.round(percent * 100);
+        document.getElementById("progress-bar").innerHTML = `${percent}%`
+        document.getElementById("progress-bar").style = `width:${percent}%`;
+
     let question = questions[currentQuestion]; // ruft den index von unserem json auf 
     document.getElementById("first-question").innerHTML = currentQuestion+1; // zeigt i von der frage an 
     document.getElementById("questiontext").innerHTML = question["question"]; // wir tun in den frage Teil die "question" von unserem json rein 
@@ -27,6 +39,7 @@ function answer(selection){ // answer ist auf onclick und die selection werden a
 
      if(selectedQuestionNumber == question["right_answer"]){ // wenn die letzte zahl true ist mit der right answer dann true 
         document.getElementById(selection).parentElement.classList.add("bg-success")
+        rightQuestions++; 
      } else{
         document.getElementById(selection).parentElement.classList.add("bg-danger")
         document.getElementById(idOfRIghtAnswer).parentElement.classList.add("bg-success") // hier übergeben wir dem right container die class
